896481f8164d92d209a095b01deb496bfad344f3
Francisco Alejandro Belmonte Flores <192310099@itslerdo.edu.mx>
2023-05-26 12:39:30 -0600

    V1

---
+++ api_belmonteflores/app/Models/Mongodb/Puntuacion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Mongodb;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Puntuacion extends Model
+{
+    use HasFactory;
+}
